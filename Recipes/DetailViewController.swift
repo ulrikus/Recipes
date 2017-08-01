8b4a15cdcc31a0fec58675a22b02c52d2fbd93e0
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-01 15:55:33 +0200

    Adds a detailViewController

---
+++ Recipes/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  Recipes
+//
+//  Created by Utheim Sverdrup, Ulrik on 01.08.2017.
+//  Copyright © 2017 Ulrik Utheim Sverdrup. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
