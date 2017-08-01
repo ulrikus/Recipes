5cd805aadff72f61de07682eabebc8aebd436886
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-01 15:32:56 +0200

    Makes a model for the recipes

---
+++ Recipes/Recipe.swift
@@ -0,0 +1,11 @@
+//
+//  Recipe.swift
+//  Recipes
+//
+//  Created by Utheim Sverdrup, Ulrik on 01.08.2017.
+//  Copyright © 2017 Ulrik Utheim Sverdrup. All rights reserved.
+//
+
+struct Recipe {
+    let title: String
+}
