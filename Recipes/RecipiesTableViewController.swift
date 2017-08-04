2027c76a715ac3e899f4f2bbf55d77266d6b4886
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-04 12:11:57 +0200

    Adds reading and writing to database

--- Recipes/RecipiesTableViewController.swift
+++ Recipes/RecipiesTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RecipiesTableViewController.swift
 //  Recipes
 //
 //  Created by Ulrik Utheim Sverdrup on 01.08.2017.
@@ -11,8 +11,10 @@ import UIKit
 class RecipiesTableViewController: UITableViewController {
 
     let segueIdentifier = "recipeDetailSegue"
+    let databaseCollection = "collection"
     var recipes = [Recipe]()
     var titleToPass: String!
+    var keyToPass: String!
     
     lazy var database: YapDatabase = {
         let database = YapDatabase(path: String.databasePath())
@@ -31,9 +33,7 @@ class RecipiesTableViewController: UITableViewController {
         self.navigationItem.rightBarButtonItem = addBarButton
         self.navigationItem.leftBarButtonItem = editButtonItem
         
-        connection.readWrite { (transaction) in
-            print(transaction.object(forKey: "world", inCollection: "example1") ?? "")
-        }
+        loadRecipes()
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,18 +58,28 @@ class RecipiesTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Get Cell Label
         let indexPath = tableView.indexPathForSelectedRow!
         let currentCell = tableView.cellForRow(at: indexPath)! as UITableViewCell
         
         titleToPass = currentCell.textLabel?.text
+        keyToPass = titleToPass + "Recipe"
         self.performSegue(withIdentifier: segueIdentifier, sender: self)
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            recipes.remove(at: indexPath.row)
+            let deletedRecipe = recipes.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            // Delete recipe from database
+            connection.readWrite { (transaction) in
+                let deleteKey = transaction.allKeys(inCollection: self.databaseCollection)[indexPath.row]
+                transaction.removeObject(forKey: deleteKey, inCollection: self.databaseCollection)
+                print(transaction.allKeys(inCollection: self.databaseCollection))
+            }
+
+            NSLog("\(deletedRecipe.title) deleted")
+            
             tableView.reloadData()
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
@@ -100,43 +110,68 @@ class RecipiesTableViewController: UITableViewController {
             let viewController = segue.destination as! DetailViewController
             // your new view controller should have property that will store passed value
             viewController.recipeTitle = titleToPass
+            viewController.databaseKeyForValue = keyToPass
+            viewController.connection = connection
+            viewController.databaseCollection = databaseCollection
         }
     }
     
     // MARK: Methods
     
     func addRecipe() {
-        self.connection.readWrite { (transaction) in
-            transaction.setObject(Date(), forKey: "world", inCollection: "example1")
+        // Create the alert controller
+        let alertController = UIAlertController(title: "Add recipe", message: "Type in the title of your recipe", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "Recipe title"
         }
-//        // Create the alert controller
-//        let alertController = UIAlertController(title: "Add recipe", message: "Type in the title of your recipe", preferredStyle: .alert)
-//        
-//        alertController.addTextField { (textField) in
-//            
-//        }
-//        
-//        // Create the actions
-//        let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.default) { alert in
-//            let title = alertController.textFields![0].text
-//            let recipe = Recipe(title: title!)
-//            
-//            self.recipes.append(recipe)
-//            self.tableView.reloadData()
-//            self.viewDidLoad()
-//            
-//            NSLog("\(title!) saved")
-//        }
-//        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { alert in
-//            NSLog("Cancel Pressed")
-//        }
-//        
-//        // Add the actions
-//        alertController.addAction(saveAction)
-//        alertController.addAction(cancelAction)
-//        
-//        // Present the controller
-//        self.present(alertController, animated: true, completion: nil)
+        
+        // Create the actions
+        let saveAction = UIAlertAction(title: "Save", style: UIAlertActionStyle.default) { alert in
+            let title = alertController.textFields![0].text
+            let recipe = Recipe(title: title!)
+            
+            if title == "" {
+                NSLog("Empty recipe title. Nothing is saved.")
+            } else {
+                self.recipes.append(recipe)
+                self.tableView.reloadData()
+                
+                // Save recipe to database
+                self.connection.readWrite { (transaction) in
+                    transaction.setObject(title, forKey: (recipe.title + "Recipe"), inCollection: self.databaseCollection)
+                    print(transaction.allKeys(inCollection: self.databaseCollection))
+                }
+                
+                NSLog("\(title!) saved")
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) { alert in
+            NSLog("Cancel Pressed")
+        }
+        
+        // Add the actions
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        // Present the controller
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func loadRecipes() {
+        // Read recipe from database
+        connection.readWrite { (transaction) in
+            let allKeys = transaction.allKeys(inCollection: self.databaseCollection)
+            print(allKeys)
+            
+            for key in allKeys {
+                guard let title = transaction.object(forKey: key, inCollection: self.databaseCollection) as? String else {
+                    return
+                }
+                let recipe = Recipe(title: title)
+                self.recipes.append(recipe)
+            }
+        }
     }
 }
 
