bfa6e9e361296d0c22f5093b3900e644793dedea
Utheim Sverdrup, Ulrik <ulrik.utheim.sverdrup@finn.no>
2017-08-11 13:44:28 +0200

    Delets storyboard and creates files to replace it

---
+++ Recipes/DetailView.swift
@@ -0,0 +1,11 @@
+//
+//  DetailView.swift
+//  Recipes
+//
+//  Created by Utheim Sverdrup, Ulrik on 11.08.2017.
+//  Copyright © 2017 Ulrik Utheim Sverdrup. All rights reserved.
+//
+
+import UIKit
+
+
